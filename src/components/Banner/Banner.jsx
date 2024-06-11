4024fd9cb6b4d015b38fc4cf9bd13fe1d293e564
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-06-11 11:48:38 +0600

    added banner section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,28 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+const Banner = ({ data }) => {
+    return (
+        <div className="max-w-6xl mx-auto border rounded-lg ">
+            <Carousel className="p-3">
+                {data.map((medicine, index) => (
+                    <div key={index}>
+
+                        <h1 className="text-4xl text-black font-semibold mt-10">{medicine.name}</h1>
+                        <h1 className="text-2xl text-black font-medium mt-3">{medicine.description}</h1>
+
+                        <img className="mt-5"
+                            style={{ maxWidth: '30%', height: 'auto' }}
+                            src={medicine.image_url}
+                            alt={medicine.name}
+                        />
+
+
+
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
