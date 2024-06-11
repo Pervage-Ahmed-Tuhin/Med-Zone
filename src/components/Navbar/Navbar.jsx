35c1ecdfffcdb9066f6af994ab25b8b382778a85
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-06-11 09:18:44 +0600

    addeed nav bar and fixed auth provider

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,79 @@
+import { NavLink } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
+import useAuth from "../Hooks/useAuth";
+const Navbar = () => {
+
+
+    const { user, LogOutUser } = useAuth();
+
+    const handleLogout = () => {
+        LogOutUser()
+            .then(result => {
+                console.log("Signed out Successfully", result);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+
+    const links = <>
+        <li className="list-none"><NavLink to='/'>Home</NavLink></li>
+        <li className="list-none"><NavLink to='/shop'>Shop</NavLink></li>
+        <li className="list-none text-xl"><NavLink to='/cart'><FiShoppingCart /></NavLink></li>
+        <select name="" id="">
+            <option value="English">English</option>
+            <option value="Bangla">Bangla</option>
+        </select>
+
+    </>
+    return (
+        <div className="navbar bg-base-100 max-w-6xl mx-auto">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {links}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">Med-Zone</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {links}
+                </ul>
+            </div>
+            {
+                user ? <>
+                    <div className="dropdown dropdown-end ml-3">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img alt="Tailwind CSS Navbar component" src={user.photoUrl} />
+                            </div>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li onClick={handleLogout}><a>Logout</a></li>
+                        </ul>
+                    </div>
+
+                </> : <>
+                    <div className="navbar-end">
+                        <a className="btn">Join Us</a>
+                    </div>
+
+                </>
+            }
+        </div>
+    );
+};
+
+export default Navbar;
--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -1,8 +1,16 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 
-import { createContext,  } from "react";
 
 
+import { GoogleAuthProvider } from "firebase/auth";
 
+const GoogleProvider = new GoogleAuthProvider();
+
+import { GithubAuthProvider } from "firebase/auth";
+import auth from "../../Firebase/firebase.config";
+
+const GithubProvider = new GithubAuthProvider();
 
 export const AuthContext = createContext(null);
 
@@ -10,10 +18,73 @@ export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
 
-    
+    const [user, setUser] = useState(null);
+    const [loader, setLoader] = useState(true);
+    const [infoHolder, setInfoHolder] = useState({});
+    const createUser = (email, password) => {
+        setLoader(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+
+    const LogOutUser = () => {
+        setLoader(true);
+        return signOut(auth);
+    }
+
+    const GoogleLogin = () => {
+        setLoader(true);
+        return signInWithPopup(auth, GoogleProvider);
+    }
+
+    const GitHUbLogin = () => {
+        setLoader(true);
+        return signInWithPopup(auth, GithubProvider);
+    }
+
+    const UpdateUserProfile = (name, photoURL) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photoURL
+        })
+            .then(() => {
+
+                // console.log("update successful");
+            })
+            .catch((error) => {
+                // console.log(error);
+            });
+    }
+
+
+    const loginUser = (email, password) => {
+        setLoader(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(() => {
+        const Unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setInfoHolder(currentUser);
+            setLoader(false);
+
+        });
+        return () => {
+            Unsubscribe();
+        }
+    }, [])
 
     const info = {
-      
+        user,
+        createUser,
+        setInfoHolder,
+        UpdateUserProfile,
+        LogOutUser,
+        loginUser,
+        GoogleLogin,
+        GitHUbLogin,
+        infoHolder,
+        loader,
+        setUser
     }
 
     return (
