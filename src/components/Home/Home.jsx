423d1bb68363b0379324a3f30fd2357ba77cc304
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-06-19 10:41:16 +0600

    made a custom hook for retriving the user role from the database

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,14 @@ import Banner from "../Banner/Banner";
 import { useLoaderData } from "react-router-dom";
 import HomeCategory from "../CategoryBox/HomeCategory";
 import DiscountBanner from "../DiscoutBanner/DiscountBanner";
+import useRole from "../Hooks/useRole";
 
 
 const Home = () => {
     const data = useLoaderData();
     console.log(data);
+    const [role,isLoading] = useRole();
+    console.log(role)
     return (
         <div className="max-w-6xl mx-auto mt-32">
             <Helmet>
